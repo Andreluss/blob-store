eaa9ed67992d906e37be038ffb142752531c1ce1
Froxyy-Dev <mateusz.winiarek1@gmail.com>
2025-01-22 18:57:41 +0100

    implement hashing using xxhash

--- src/common/blob_hasher.hpp
+++ src/common/blob_hasher.hpp
@@ -1,30 +1,40 @@
 #pragma once
 
 #include <string>
+#include "stdlib.h"   /* abort() */
+#include "xxhash.h"
 
-/// Incremental hashing for blob chunks. Example usage:
+/// Incremental hashing for blob chunks.
+/// + faster (50GB/s vs 0.5GB/s)
+/// - non-cryptographic, more collisions
+/// https://github.com/Cyan4973/xxHash
+/// Example usage:
 ///   BlobHasher blob_hasher;
 ///   blob_hasher.add_chunk("blabla");
 ///   blob_hasher.add_chunk("hello");
 ///   std::string output_hash = blob_hasher.finalize();
 class BlobHasher {
-    std::string hash_ = "UGABUGABUGABOO";
-    int ptr = 0;
+    XXH64_state_t* state;
 public:
+    /// TODO: We probably should set seed in config.
+    BlobHasher(XXH64_hash_t seed = 0) {
+        state = XXH64_createState();
+        if (state==NULL) abort();
+
+        /* Initialize state with selected seed */
+        if (XXH64_reset(state, seed) == XXH_ERROR) abort();
+    }
+
     void add_chunk(const std::string& bytes) {
-        for (int i = 0; i < bytes.size(); i++) {
-            ptr = (ptr + bytes[i]) % hash_.size();
-            hash_[ptr] ^= bytes[i];
-        }
+        if (XXH64_update(state, bytes.c_str(), bytes.size()) == XXH_ERROR) abort();
     }
 
     /// Return the hash of all data. Call ONLY ONCE per object.
-    std::string finalize() {
-        return hash_;
+    /// typedef uint64_t XXH64_hash_t;
+    XXH64_hash_t finalize() {
+        /* Produce the final hash value */
+        XXH64_hash_t const hash = XXH64_digest(state);
+        XXH64_freeState(state);
+        return hash;
     }
 };
-
-// TODO: Implement hashing with xxHash https://github.com/Cyan4973/xxHash
-// + faster (50GB/s vs 0.5GB/s) 
-// - non-cryptographic, more collisions
-// Alternative: SHA-256 using OpenSSL or Crypto++ library.
