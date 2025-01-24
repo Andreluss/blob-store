04416923da09a7dabdb970baac640c6bf989e668
Andrzej Jablonski <andrzoff@gmail.com>
2025-01-24 23:15:08 +0100

    sample client upload blob

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -38,6 +38,8 @@
         auto channel = grpc::CreateChannel(frontend_load_balancer_address, grpc::InsecureChannelCredentials());
         const auto frontend_stub = frontend::Frontend::NewStub(std::move(channel));
 
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
         std::cout << "Uploading blob...\n";
 
         grpc::ClientContext client_ctx;
@@ -51,29 +53,34 @@
             std::string raw_blob {"ABRAKADABRA"};
             std::cout << "Starting blob upload! | OK " << std::endl;
 
-            frontend::BlobInfo blob_info;
-            blob_info.set_size_bytes(raw_blob.size());
-            request.set_allocated_info(&blob_info);
+            auto* blob_info = new frontend::BlobInfo;
+            blob_info->set_size_bytes(raw_blob.size());
+            request.set_allocated_info(blob_info);
 
             if (not writer->Write(request))
             {
                 std::cerr << "Failed to upload blob HEADER!" << std::endl;
-                std::cerr << response.SerializeAsString() << std::endl;
             } else
             {
                 std::cerr << "Sent blob header! | OK " << std::endl;
 
+                request.set_chunk_data(raw_blob);
+                if (not writer->Write(request))
                 {
-                    request.set_chunk_data(raw_blob);
-                    if (not writer->Write(request))
+                    std::cerr << "Failed to upload blob DATA!" << std::endl;
+                }
+                else
+                {
+                    std::cerr << "Sent blob data! | OK " << std::endl;
+                    writer->WritesDone();
+
+                    if (auto status = writer->Finish(); status.ok())
                     {
-                        std::cerr << "Failed to upload blob DATA!" << std::endl;
-                        std::cerr << response.SerializeAsString() << std::endl;
+                        std::cerr << "DONE - status OK" << std::endl;
                     }
                     else
                     {
-                        std::cerr << "Sent blob data! | OK " << std::endl;
-                        std::cerr << response.SerializeAsString() << std::endl;
+                        std::cerr << "DONE - status " << status.error_code() << " " << status.error_message() << std::endl;
                     }
                 }
             }
