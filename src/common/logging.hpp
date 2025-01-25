6d28092cbe394c3b5f54401b11b925cbcb1826e2
Andrzej Jablonski <andrzoff@gmail.com>
2025-01-25 21:13:38 +0100

    logger

--- src/common/logging.hpp
+++ src/common/logging.hpp
@@ -9,18 +9,17 @@
 
 // ----------------------------------- Common classes -----------------------------------
 
-struct Color {
-    static constexpr const char* Red = "\033[31m";
-    static constexpr const char* Green = "\033[32m";
-    static constexpr const char* Yellow = "\033[33m";
-    static constexpr const char* Blue = "\033[34m";
-    static constexpr const char* Magenta = "\033[35m";
-    static constexpr const char* Cyan = "\033[36m";
-    static constexpr const char* Reset = "\033[0m";
-};
-
 class Logger {
-private:
+    struct Color {
+        static constexpr const char* Red = "\033[31m";
+        static constexpr const char* Green = "\033[32m";
+        static constexpr const char* Yellow = "\033[33m";
+        static constexpr const char* Blue = "\033[34m";
+        static constexpr const char* Magenta = "\033[35m";
+        static constexpr const char* Cyan = "\033[36m";
+        static constexpr const char* Reset = "\033[0m";
+    };
+
     // Function that returns the string with time in a format like this: 2024-04-25T18:21:00.010 (with parts of seconds)
     // https://gist.github.com/bschlinker/844a88c09dcf7a61f6a8df1e52af7730
     static std::string getCurrentTime() {
@@ -33,24 +32,22 @@ private:
               << '.' << std::setfill('0') << std::setw(3) << nowMs.count();
         return nowSs.str();
     }
-public:
-    static void debug(std::string color, std::string message) {
-        std::cerr << color << message << Color::Reset << std::endl << std::flush;
-    }
-    static void info(const char* color, std::string message) {
-        std::cerr << color << message << Color::Reset << std::endl;
-    }
-    static void error(std::string message) {
-        std::cerr << Color::Red << "[Error] " << Color::Reset << message << std::endl;
-    }
-    static void warn(std::string message) {
-        std::cerr << Color::Yellow << "[Warning] " << Color::Reset << message << std::endl;
+
+    static void _print(const std::string color, const std::string type, const std::string& message) {
+        std::cerr << getCurrentTime() << " " << color << "[" << type << "]" << Color::Reset << " " << message << std::endl;
     }
 
-    static void report(const std::string &senderIpPort, const std::string &receiverIpPort,
-                       const std::string &time, const std::string &message) {
-        if (BlackLadyDebug) std::cerr << std::flush;
-        std::cout << "[" << senderIpPort << "," << receiverIpPort << "," << time << "] " << message << std::flush; // << std::endl;
-        if (BlackLadyDebug) std::cerr << std::flush;
+public:
+    static void debug(std::string message) {
+        _print(Color::Cyan, "Debug", message);
+    }
+    static void info(std::string message) {
+        _print(Color::Green, "Info", message);
+    }
+    static void error(std::string message) {
+        _print(Color::Red, "Error", message);
+    }
+    static void warn(std::string message) {
+        _print(Color::Yellow, "Warn", message);
     }
 };
